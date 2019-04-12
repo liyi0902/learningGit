dac31f74a85ae31ec45d8ce33b1507df0bd29a67
liyi <liyi0902@qq.com>
2019-04-12 20:29:46 +1000

    change twitter data structure to dict

--- spartan/sortTwitter.py
+++ spartan/sortTwitter.py
@@ -231,7 +231,7 @@ comm=MPI.COMM_WORLD
 comm_rank=comm.Get_rank()
 comm_size=comm.Get_size()
 file_path='melbGrid.json'
-file_path2='smallTwitter.json'
+file_path2='bigTwitter.json'
 region=extractFromGrid(file_path)
 
 if comm_rank==0:
